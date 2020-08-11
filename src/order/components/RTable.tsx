50120ab19a7265761e8545d672236239990fe2e9
Rainbow <rainbow1071@163.com>
2020-08-11 23:16:04 +0800

    订单详情

--- src/order/components/RTable.tsx
+++ src/order/components/RTable.tsx
@@ -4,13 +4,37 @@ import { Table } from 'antd';
 interface Columns {
     [columns: string]: any,
 }
+const rowSelection = {
+    onChange: (selectedRowKeys: any, selectedRows: any) => {
+        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    },
+    // getCheckboxProps: (record: any) => ({
+    //     disabled: record.name === 'Disabled User', // Column configuration not to be checked
+    //     name: record.name,
+    // }),
+};
 
-class RTable extends Component <Columns>{
-    
+class RTable extends Component<Columns>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
     render() {
         console.log(this.props)
         return (
-            <Table columns={this.props.columns} dataSource={this.props.dataSource} />
+            <div>
+                <Table
+                    rowSelection={{
+                        type: 'checkbox',
+                        ...rowSelection,
+                    }}
+                    columns={this.props.columns}
+                    dataSource={this.props.dataSource}
+                />
+            </div>
         )
     }
 }
--- src/order/index.tsx
+++ src/order/index.tsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import RTable from './components/RTable'
-import Radio from './components/Radio'
+import RTable from './components/RTable';
+import Radio from './components/Radio';
+import { Space, Button } from 'antd';
 import './index.scss'
 
 const columns = [
     {
         title: '订单编号',
-        dataIndex: 'avatar',
-        key: 'avatar',
+        dataIndex: 'number',
+        key: 'number',
     },
     {
         title: '提交时间',
@@ -17,8 +18,8 @@ const columns = [
 
     {
         title: '用户账号',
-        key: 'count',
-        dataIndex: 'count',
+        key: 'account',
+        dataIndex: 'account',
     },
     {
         title: '订单金额',
@@ -27,23 +28,143 @@ const columns = [
     },
     {
         title: '支付方式',
-        key: 'money',
-        dataIndex: 'money',
+        key: 'payMethod',
+        dataIndex: 'payMethod',
     },
     {
         title: '订单来源',
-        key: 'money',
-        dataIndex: 'money',
+        key: 'source',
+        dataIndex: 'source',
     },
     {
         title: '订单状态',
-        key: 'money',
-        dataIndex: 'money',
+        key: 'status',
+        dataIndex: 'status',
     },
     {
         title: '操作',
         key: 'money',
         dataIndex: 'money',
+        render: (text: string) => (
+            <Space size="middle">
+                <Button type="link" >
+                    查看详情
+                </Button>
+            </Space>
+        ),
+    },
+]
+
+const data = [
+    {
+        key: '1',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '2',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '3',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '4',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '5',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '6',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '7',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '8',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '9',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
+    },
+    {
+        key: '10',
+        number: '123',
+        time: '2020-8-11',
+        account: 'Rainbow',
+        money: '300',
+        payMethod: '微信',
+        source: '北京',
+        status: '待发货'
+
     },
 ]
 
@@ -53,13 +174,13 @@ class OrderList extends Component {
     render() {
         return (
             <div className='order-list'>
-                <h1>订单列表</h1>
+                <h1 className='order-title'>订单列表</h1>
                 <div className='table-box'>
-                    <div>
+                    <div className='filter'>
                         <Radio />
                     </div>
                     <div>
-                        <RTable columns={columns} />
+                        <RTable columns={columns} dataSource={data} />
                     </div>
                 </div>
             </div>
